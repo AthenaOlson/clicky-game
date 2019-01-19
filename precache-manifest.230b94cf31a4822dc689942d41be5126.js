1889bebddf3931409a090b0bf1c13c892d2f63ee
Leeland Clenista <leeland_clenista@yahoo.com>
2019-01-19 08:07:23 -0800

    Updates

---
+++ precache-manifest.230b94cf31a4822dc689942d41be5126.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "adb71af6ceb28f34875a",
+    "url": "/clicky-game/static/js/main.adb71af6.chunk.js"
+  },
+  {
+    "revision": "25e9faa4f4f8a8f59577",
+    "url": "/clicky-game/static/js/1.25e9faa4.chunk.js"
+  },
+  {
+    "revision": "adb71af6ceb28f34875a",
+    "url": "/clicky-game/static/css/main.3d1a02d4.chunk.css"
+  },
+  {
+    "revision": "8a9bdee084c97c48424b1530c2ba0efa",
+    "url": "/clicky-game/index.html"
+  }
+];
